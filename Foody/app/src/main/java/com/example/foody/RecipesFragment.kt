4c6fd04843e1d619cad78092a4b7d7874ce25ecb
Nikola Kostov <niko_kostov@yahoo.com>
2021-07-15 01:27:19 +0200

    - Shimmer recycler implemented

--- Foody/app/src/main/java/com/example/foody/RecipesFragment.kt
+++ Foody/app/src/main/java/com/example/foody/RecipesFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_recipes.view.*
+
 class RecipesFragment : Fragment() {
 
     override fun onCreateView(
@@ -12,7 +14,11 @@ class RecipesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipes, container, false)
+        val view = inflater.inflate(R.layout.fragment_recipes, container, false)
+
+        view.recyclerView.showShimmer()
+
+        return view
     }
 
 }
