ee8bdf32731062946b2be1444cbd10e1e0ae9a7d
Nikola Kostov <niko_kostov@yahoo.com>
2021-07-15 02:26:06 +0200

    - Project structure, API response classes, constants class implementation

---
+++ Foody/app/src/main/java/com/example/foody/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.foody
+
+class Constants {
+
+    companion object{
+        const val API_KEY = "03304cf9ec20471e8fcaf17df6a06775"
+        const val BASE_URL = "https://api.spoonacular.com"
+    }
+}
