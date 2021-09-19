8380c9526ca4cc9537286eaa7d8c1ec01b1c2bfe
nikolatasevski2 <nikolatasevski14@gmail.com>
2021-09-19 14:18:46 +0200

    account fragment layout setup

--- Foody/app/src/main/java/com/example/foody/ui/MainActivity.kt
+++ Foody/app/src/main/java/com/example/foody/ui/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : AppCompatActivity() {
         val appBarConfiguration = AppBarConfiguration(setOf(
             R.id.recipesFragment,
             R.id.favoriteRecipesFragment,
-            R.id.foodJokeFragment
+            R.id.accountFragment
         ))
 
         bottomNavigationView.setupWithNavController(navController)
--- Foody/app/src/main/java/com/example/foody/ui/fragments/account/AccountFragment.kt
+++ Foody/app/src/main/java/com/example/foody/ui/fragments/account/AccountFragment.kt
@@ -1,4 +1,4 @@
-package com.example.foody.ui.fragments.foodjoke
+package com.example.foody.ui.fragments.account
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,14 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.foody.R
 
-class FoodJokeFragment : Fragment() {
+class AccountFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_food_joke, container, false)
+        return inflater.inflate(R.layout.fragment_account, container, false)
     }
 
 }
